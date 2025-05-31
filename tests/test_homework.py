be10d386e18dd63ddad0881a233b9104a2e20429
Karen Rivera Giraldo <kariverag@unal.edu.co>
2025-05-31 07:08:01 -0500

    Corrección test

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -12,9 +12,7 @@ def test_01():
     # Test if the homework script runs without errors
     try:
         subprocess.run(
-            [
-                "mlflow run https://github.com/jdvelasq/mlflow-wine-quality.git -e knn -P n_neighbors=5"
-            ],
+            ["bash", "run.sh"],
             check=True,
         )
     except subprocess.CalledProcessError as e:
